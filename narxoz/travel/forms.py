19b82f1ca3d42204adf49c13749745468611396e
balnuryertay <ertaibalnur@icloud.com>
2023-03-12 13:58:54 +0600

    commit

--- narxoz/travel/forms.py
+++ narxoz/travel/forms.py
@@ -1,10 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="title")
-    slug = forms.SlugField(max_length=255, label="url")
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="text")
-    is_published = forms.BooleanField(label="posts", required=False, initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="categories", empty_label="категория таңдалынбады")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Катергория таңдалмады"
 
+    class Meta:
+        model = Travel
+        fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-input'}),
+            'content': forms.Textarea(attrs={'cols':68, 'roms': 10}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Ұзындық 200-ден асып кетті')
+
+        return title
--- narxoz/travel/views.py
+++ narxoz/travel/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseServerError, HttpResponseForbidden, \
     HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -11,33 +13,56 @@ menu = [
     {'title':"Байланыс", 'url_name':'contact'},
     {'title':"Логин", 'url_name':'login'}]
 
-def index(request):
-    posts = Travel.objects.all()
+class TravelHome(ListView):
+    model = Travel
+    template_name = 'travel/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Travel Land',
-        'cat_selected': 0,
-    }
-    return render(request, 'travel/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Басты бет'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Travel.objects.filter(is_published=True)
+
+# def index(request):
+#     posts = Travel.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Travel Land',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'travel/index.html', context=context)
 
 def about(request):
     return render(request, 'travel/about.html', {'menu': menu, 'title': 'About us'})
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            try:
-                Travel.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Пост құруда қателіктер кетті')
-    else:
-        form = AddPostForm()
-    return render(request, 'travel/addpage.html', {'form': form, 'menu': menu, 'title': 'Жаңалықтар'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'travel/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Мақала қосу'
+        context['menu'] = menu
+        return context
+
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'travel/addpage.html', {'form': form, 'menu': menu, 'title': 'Жаңалықтар'})
 
 def contact(request):
     return HttpResponse('Обратная связь')
@@ -57,28 +82,57 @@ def forbidden(request, exception):
 def badRequest(request, exception):
     return HttpResponseBadRequest('<h1>400<br>Невозможно обработать запрос</h1>')
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Travel, slug=post_slug)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Travel, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'travel/post.html', context=context)
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
+class ShowPost(DetailView):
+    model = Travel
+    template_name = 'travel/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    return render(request, 'travel/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
 
-def show_category(request, cat_id):
-    posts = Travel.objects.filter(cat_id=cat_id)
+class TravelCategory(ListView):
+    model = Travel
+    template_name = 'travel/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Travel.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Категория бойынша көрсету',
-        'cat_selected': cat_id,
-    }
-    return render(request, 'travel/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+# def show_category(request, cat_id):
+#     posts = Travel.objects.filter(cat_id=cat_id)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Категория бойынша көрсету',
+#         'cat_selected': cat_id,
+#     }
+#     return render(request, 'travel/index.html', context=context)
